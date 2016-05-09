77c3e178fc67b9de9984b1ff366b6d1bc7fb58c7
Victor <victormel87@gmail.com>
2016-05-09 12:50:56 +0200

    Completada clase MessagePost

--- MessagePost.java
+++ MessagePost.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Write a description of class MessagePost here.
  * 
@@ -7,27 +7,88 @@
  */
 public class MessagePost
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private String username;
+    private String message;
+    private long timestamp;
+    private int likes;
+    private ArrayList<String> comments;
 
     /**
      * Constructor for objects of class MessagePost
      */
-    public MessagePost()
+    public MessagePost(String author, String text)
     {
-        // initialise instance variables
-        x = 0;
+       this.username = author;
+       this.message = text;
+       this.timestamp = System.currentTimeMillis();
+       likes = 0;
+       comments = new ArrayList<>();
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Metodo para dar un like.
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void like(){
+        likes++;
+    }
+    
+    /**
+     * Metodo para quitar un like en caso de que los haya.
+     */
+    public void unlike(){
+        if(likes != 0)
+           likes--; 
+    }
+    
+    /**
+     * Metodo para añadir un comentario al post
+     */
+    public void addComment(String text){
+        comments.add(text);
+    }
+    
+    /**
+     * Metodo que devuelve el mensaje del post
+     */
+    public String getText(){
+        return message;
+    }
+    
+    /**
+     * Metodo que devuelve la estampa de tiempo en el momento de crear el post
+     */
+    public long getTimeStamp(){
+        return timestamp;
+    }
+    
+    /**
+     * Metodo que muestra toda la info del post
+     */
+    public void display(){
+        String info = "";
+        long time = System.currentTimeMillis() - getTimeStamp();
+        info += username + "\n=====================\n" + "Posted: ";
+        info += timeString(time);
+        info += "=====================\n" + getText() + "\n";
+        info += "_____________________\nLikes: " + likes + "\n=====================\n\n";
+        if(comments.size() != 0)
+            for(int i=0; i<comments.size(); i++)
+                info += comments.get(i) + "\n_____________________\n";
+        else
+            info += "Sin comentarios";
+        System.out.println(info);
+    }
+    
+    /**
+     * Metodo para pasar el tiempo a minutos y segundos.
+     */
+    private String timeString(long time){
+        String info = "";
+        int sec =  (int)(time / 1000) % 60;
+        int min =  (int)((time / (1000*60)) % 60);
+        if(min > 0)
+            info += min + " Minutes, ";
+        info += sec + " Seconds\n";
+        return info;
     }
 }
